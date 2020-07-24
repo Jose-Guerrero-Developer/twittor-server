829c42196293ef8398c40500a8064094c01407ae
Jose Alexander Guerrero Carrizo <jose.guerrero.carrizo@gmail.com>
2020-07-24 15:19:25 -0500

    Estableciendo configuración de servidor y base de datos

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"log"
+
+	"github.com/Jose-Guerrero-Developer/twittorbackend/database"
 	"github.com/Jose-Guerrero-Developer/twittorbackend/handlers"
 )
 
 func main() {
-	// status, error := database.CheckConnectionStatus()
-	// if !status {
-	// 	log.Fatal(error)
-	// }
+	status, error := database.CheckConnectionStatus()
+	if !status {
+		log.Fatal(error)
+	}
 	handlers.Init()
 }

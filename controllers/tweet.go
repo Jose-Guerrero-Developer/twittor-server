d2947d69f7096102112b928f1d40f1996b3a921e
José Guerrero <jose.guerrero.carrizo@gmail.com>
2020-08-03 11:27:08 -0500

    Creación enpoint Tweet
    Se crea los puntos de rutas y insertar un tweet

---
+++ controllers/tweet.go
@@ -0,0 +1,48 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson"
+
+	"github.com/Jose-Guerrero-Developer/twittorbackend/utils"
+
+	"github.com/Jose-Guerrero-Developer/twittorbackend/models"
+)
+
+/*TweetController tweet controller */
+type TweetController struct{}
+
+/*Store tweetrecord invoegen in database */
+func (Controller *TweetController) Store(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var err error
+	var status bool
+	var Tweet models.Tweet
+	var Profile models.Profile
+	err = json.NewDecoder(r.Body).Decode(&Tweet)
+	if err != nil {
+		utils.ResponseFailed(w, "001", "Error getting data", err.Error(), http.StatusBadRequest)
+		return
+	}
+	if len(Tweet.IDProfile) <= 0 {
+		utils.ResponseFailed(w, "011", "Required parameter", "It is necessary to send in the application an id profile", http.StatusBadRequest)
+		return
+	}
+	Profile.ID = Tweet.IDProfile
+	if !Profile.ExistsID() {
+		utils.ResponseFailed(w, "012", "Resource in the found", "Profile id not found", http.StatusNotFound)
+		return
+	}
+	if len(Tweet.Message) <= 0 {
+		utils.ResponseFailed(w, "011", "Required parameter", "It is necessary to send in the application an message", http.StatusBadRequest)
+		return
+	}
+	status, err = Tweet.Store()
+	if !status || err != nil {
+		utils.ResponseFailed(w, "005", "Database transaction", "Error storing tweet data. "+err.Error(), http.StatusConflict)
+		return
+	}
+	utils.Response(w, bson.M{}, http.StatusCreated)
+}
---
+++ models/tweet.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+/*Tweet structure to manage tweet model */
+type Tweet struct {
+	IDProfile primitive.ObjectID `bson:"_id_profile" json:"idProfile"`
+	Message   string             `bson:"message" json:"message"`
+	CreatedAt time.Time          `bson:"created_at" json:"createdAt"`
+}
+
+/*Store register a tweet in the database */
+func (Model *Tweet) Store() (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	Tweets := ORM.Collection("tweets")
+	Model.CreatedAt = time.Now()
+	_, err := Tweets.InsertOne(ctx, Model)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}

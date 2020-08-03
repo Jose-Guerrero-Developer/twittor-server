22f96f25b03867e1925d70c617eb44e0be527d31
José Guerrero <jose.guerrero.carrizo@gmail.com>
2020-08-03 14:23:50 -0500

    Simplificando sintaxis en controlador de tweet
    Mejorando la sintaxis en el controlador tweet

--- controllers/tweet.go
+++ controllers/tweet.go
@@ -17,20 +17,13 @@ type Tweet struct{}
 /*Store tweetrecord invoegen in database */
 func (Controller *Tweet) Store(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var err error
-	var status bool
 	var Tweet models.Tweet
 	var Profile models.Profile
-	err = json.NewDecoder(r.Body).Decode(&Tweet)
-	if err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&Tweet); err != nil {
 		utils.ResponseFailed(w, "001", "Error getting data", err.Error(), http.StatusBadRequest)
 		return
 	}
 	Tweet.IDProfile = Profile.GetID()
-	if len(Tweet.IDProfile) <= 0 {
-		utils.ResponseFailed(w, "011", "Required parameter", "It is necessary to send in the application an id profile", http.StatusBadRequest)
-		return
-	}
 	if !Profile.ExistsID() {
 		utils.ResponseFailed(w, "012", "Resource in the found", "Profile id not found", http.StatusNotFound)
 		return
@@ -39,8 +32,7 @@ func (Controller *Tweet) Store(w http.ResponseWriter, r *http.Request) {
 		utils.ResponseFailed(w, "011", "Required parameter", "It is necessary to send in the application an message", http.StatusBadRequest)
 		return
 	}
-	status, err = Tweet.Store()
-	if !status || err != nil {
+	if status, err := Tweet.Store(); !status || err != nil {
 		utils.ResponseFailed(w, "005", "Database transaction", "Error storing tweet data. "+err.Error(), http.StatusConflict)
 		return
 	}

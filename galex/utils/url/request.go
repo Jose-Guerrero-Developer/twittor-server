6a996c6050700d37933609e8634e2d5e690eabd1
José Guerrero <jose.guerrero.carrizo@gmail.com>
2020-08-05 11:45:14 -0500

    Método GetHeader
    Nuevo método GetHeader para retorna un parámetro enviado por cabecera

--- galex/utils/url/request.go
+++ galex/utils/url/request.go
@@ -6,3 +6,8 @@ import "net/http"
 func (Controller *Driver) GetRequest() *http.Request {
 	return _Context.Request
 }
+
+/*GetHeader Returns one sent parameter per header */
+func (Controller *Driver) GetHeader(key string) string {
+	return _Context.Request.Header.Get(key)
+}

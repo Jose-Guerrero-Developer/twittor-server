ee2bbf7986a51e834eaa923b1eee1c0f2d3ced58
José Guerrero <jose.guerrero.carrizo@gmail.com>
2020-08-06 23:30:44 -0500

    Soporte para agregar encabezados de respuesta
    Agregamos soporte de encabezados de respuestas en los endpoint

--- galex/utils/pagination/params.go
+++ galex/utils/pagination/params.go
@@ -6,6 +6,12 @@ import (
 	"github.com/Jose-Guerrero-Developer/twittorbackend/galex/utils/url"
 )
 
+/*AddHeader Add header */
+func (Controller *Driver) AddHeader(name string, value string) {
+	var GalexURL url.Driver
+	GalexURL.AddHeader(name, value)
+}
+
 /*GetPage Returns the page number to be displayed */
 func (Controller *Driver) GetPage() int64 {
 	var GalexURL url.Driver

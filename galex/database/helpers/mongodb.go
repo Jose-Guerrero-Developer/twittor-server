476aec33dc8e95af7d50a1516ffa92df362b4865
José Guerrero <jose.guerrero.carrizo@gmail.com>
2020-08-10 15:25:23 -0500

    Cambios para obtener la collección
    Obtener la collección de datos

--- galex/database/helpers/mongodb.go
+++ galex/database/helpers/mongodb.go
@@ -12,48 +12,39 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-/*Collection returns a collection from the database */
-func (Helper *Driver) Collection(name string) *mongo.Collection {
+/*GetCollection returns a collection from the database */
+func (Helper *Driver) GetCollection() *mongo.Collection {
 	var GalexDatabase database.Driver
-	return GalexDatabase.Client().Database(GalexDatabase.GetDatabaseName()).Collection(name)
+	return GalexDatabase.Client().Database(GalexDatabase.GetDatabaseName()).Collection(Helper.collection)
 }
 
 /*Find Mongo Find */
-func (Helper *Driver) Find(ctx context.Context, table string, filter bson.M) (*mongo.Cursor, error) {
+func (Helper *Driver) Find(ctx context.Context, filter bson.M) (*mongo.Cursor, error) {
 	var GalexRequest request.Driver
-	var GalexDatabase database.Driver
-
-	collection := GalexDatabase.Client().Database(GalexDatabase.GetDatabaseName()).Collection(table)
 	opts := options.Find()
 	opts.SetLimit(GalexRequest.GetCount())
 	opts.SetSort(bson.D{{Key: "created_at", Value: -1}})
 	opts.SetSkip((GalexRequest.GetPage() - 1) * GalexRequest.GetCount())
 	GalexRequest.AddHeader("X-Current-Page", strconv.FormatInt(GalexRequest.GetPage(), 10))
-	return collection.Find(ctx, filter, opts)
+	return Helper.GetCollection().Find(ctx, filter, opts)
 }
 
 /*FindOne Mongo FindOne */
-func (Helper *Driver) FindOne(ctx context.Context, table string, filter bson.M) *mongo.SingleResult {
-	var GalexDatabase database.Driver
-
-	collection := GalexDatabase.Client().Database(GalexDatabase.GetDatabaseName()).Collection(table)
-	return collection.FindOne(ctx, filter)
+func (Helper *Driver) FindOne(ctx context.Context, filter bson.M) *mongo.SingleResult {
+	return Helper.GetCollection().FindOne(ctx, filter)
 }
 
 /*InsertOne Mongo InsertOne */
-func (Helper *Driver) InsertOne(ctx context.Context, table string, document interface{}) (*mongo.InsertOneResult, error) {
-	collection := Helper.Collection(table)
-	return collection.InsertOne(ctx, document)
+func (Helper *Driver) InsertOne(ctx context.Context, document interface{}) (*mongo.InsertOneResult, error) {
+	return Helper.GetCollection().InsertOne(ctx, document)
 }
 
 /*UpdateOne Mongo UpdateOne */
-func (Helper *Driver) UpdateOne(ctx context.Context, table string, filter bson.M, update bson.M) (*mongo.UpdateResult, error) {
-	collection := Helper.Collection(table)
-	return collection.UpdateOne(ctx, filter, update)
+func (Helper *Driver) UpdateOne(ctx context.Context, filter bson.M, update bson.M) (*mongo.UpdateResult, error) {
+	return Helper.GetCollection().UpdateOne(ctx, filter, update)
 }
 
 /*DeleteOne Mongo DeleteOne */
-func (Helper *Driver) DeleteOne(ctx context.Context, table string, filter bson.M) (*mongo.DeleteResult, error) {
-	collection := Helper.Collection(table)
-	return collection.DeleteOne(ctx, filter)
+func (Helper *Driver) DeleteOne(ctx context.Context, filter bson.M) (*mongo.DeleteResult, error) {
+	return Helper.GetCollection().DeleteOne(ctx, filter)
 }
